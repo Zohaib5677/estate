2730e9ff409714fe8087ec50a961a31e59b4a896
Zohaib5677 <zohaib5677@gmail.com>
2026-03-14 11:36:58 +0500

    add

--- api/index.js
+++ api/index.js
@@ -8,7 +8,8 @@ import authRoutes from './routes/auth.js';
 import uploadRoutes from './routes/upload.js';
 import listingrouter from './routes/listing.js';
 import cookieParser from "cookie-parser";
-
+import path from 'path';
+dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -17,12 +18,30 @@ app.use(cors({
   origin: 'http://localhost:5173',  // frontend URL
   credentials: true                 // allow cookies/credentials
 }));
+ const __dirname = path.resolve();
 app.use(express.json());
 app.use(cookieParser());  
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
 app.use("/api/listings", listingrouter);
 app.use("/api/users",   userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api", uploadRoutes);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
